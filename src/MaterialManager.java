9798bc7e174fb7d3a0aa6eb7dd6d98cd540188e8
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-13 15:20:07 +0700

    minor change

--- src/MaterialManager.java
+++ src/MaterialManager.java
@@ -19,7 +19,8 @@ public class MaterialManager {
     public double getTotalCost() {
         double total = 0;
         for (Material material : materials) {
-            total += material.getCost();
+            if (material instanceof Discount)
+                total += ((Discount) material).getRealCost();
         }
         return total;
     }
